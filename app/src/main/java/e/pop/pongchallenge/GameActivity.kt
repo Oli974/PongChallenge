9e4cd8288937f5fb10dd3c4e34e99dbc2ddb3aab
LEBEAU <olivier.lebeau97@gmail.com>
2019-03-28 19:42:19 +0400

    25/03/2019

---
+++ app/src/main/java/e/pop/pongchallenge/GameActivity.kt
@@ -0,0 +1,21 @@
+package e.pop.pongchallenge
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.*
+
+class GameActivity:AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState : Bundle?){
+        super.onCreate(savedInstanceState)
+
+        //set FullScreen
+        this.window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        //Set no Title
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE)
+
+        setContentView(R.layout.game_main)
+    }
+
+
+}
